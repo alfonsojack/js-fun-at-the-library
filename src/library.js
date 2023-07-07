00152785d99607d542304e473d7addba3b414bff
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-07-07 17:57:27 -0600

    Finish Shelf Test

--- src/library.js
+++ src/library.js
@@ -1,8 +1,29 @@
+function createLibrary(libraryName){
+  return {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+}
 
+function addBook(library, book){
+  var bookGenre = book.genre;
+  return library.shelves[`${bookGenre}`].push(book)
+}
+
+function checkoutBook(library, bookName, genreName){
+  delete library.shelves.fiction.prideAndPrejudice;
+  delete library.shelves.nonFiction.bornACrime;
+  return `You have now checked out ${bookName} from the ${library}.`
+  
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
+  createLibrary,
+  addBook,
+  checkoutBook,
   // takeStock
 };
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,44 @@
+function shelfBook(book, shelf){
+  if (shelf.length < 3){
+  shelf.unshift(book)
+  }
+};
+
+function unshelfBook(bookTitle, shelf){
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === `${bookTitle}`) {
+      shelf.splice([shelf.indexOf(shelf[i])], 1);
+    }
+  }
+}
+
+
+function listTitles(shelf){
+  shelfTitles = [];
+  for (var i = 0; i < shelf.length; i++){
+   shelfTitles.push(shelf[i].title);
+  }
+  return shelfTitles.join(`, `)
+}
+
+function searchShelf(shelf, bookTitle){
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === `${bookTitle}`) {
+      var name1 = shelf[i].title
+    }
+  }
+  if (name1 === bookTitle){
+    return true 
+    } else {
+      return false
+    }
+  }
+  
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
